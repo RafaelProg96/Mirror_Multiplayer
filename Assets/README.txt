2fc8f78aa7648e729d07af1d8839506dd7ad03ba
vogtguilherme <vogt.guilherme@gmail.com>
2020-03-27 12:23:58 -0300

    Comentários no código

---
+++ Assets/README.txt
@@ -0,0 +1,6 @@
+1. Coloque a cena Online no build settings
+2. Certifique-se de não existe um jogador já na cena
+
+Ao executar, você pode apertar a tecla "espaço"
+para ativar a animação da luz, e apertar "t" para
+alterar a cor do personagem.
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,20 +9,23 @@ namespace Prototipo.Multiplayer
     {
         private Renderer m_Renderer;            //Referência para o componente de renderização do objeto
         private NetworkIdentity m_Identity;     //Referência para o componente "NetworkIdentity"
-        [SyncVar(hook = "SetColor")]
-        private Color playerColor;
+		private Animator m_Animator;            //Referência para o componente "Animator"
 
-		private Animator m_Animator;
+		private Material cachedMaterial;		//Referência para alocar temporariamente um material.
 
+		//Variável do tipo Color que possui o atributo "SyncVar".
+		//Essa variável terá seu valor sincronizado com o servidor e todos os clientes conectados.
+		//Dentro do atributo temos um hook, que invoca a função do parâmetro, no momento em que a variável tem seu valor alterado.
+		[SyncVar(hook = "SetColor")] private Color playerColor;		
+		
+		//Propriedade write-only para o campo "playerColor"
 		public Color PlayerColor
 		{
 			set
 			{
 				playerColor = value;
 			}
-		}
-
-        private Material cachedMaterial;
+		}        
 
         private void Awake()
         {
@@ -40,40 +43,24 @@ namespace Prototipo.Multiplayer
 			{
 				m_Animator.SetTrigger("SwitchLight");
 			}
+
+			if(Input.GetKeyDown(KeyCode.T))
+			{
+				playerColor = RandomColor();
+			}
 		}
 
 		//Função chamada ao Spawn de um cliente na cena
 		public override void OnStartLocalPlayer()
         {
             base.OnStartLocalPlayer();
-            //Criando uma variável do tipo Color e recebendo o retorno da função RandomColor()
+            //Atribuir a "playerColor" o retorno da função RandomColor()
             playerColor = RandomColor();
-            //Chamar a função com atributo Command, que será executada no servidor
-            //CmdChangeColor();
         }
-		/*
-        //Função que será invocada no objeto do cliente e executada somente no servidor
-        [Command]
-        private void CmdChangeColor()
-        {
-            //Chama a função SetColor, passando como parâmetro a variável "playerColor"
-            SetColor(playerColor);
-            //Invoca a função Rpc no servidor, para executar em todos os clientes
-            RpcChangeColor(playerColor);
-        }
-
-        //Função que será invocada no objeto do servidor e executada em todos os clientes que possuírem essa classe
-        [ClientRpc]
-        private void RpcChangeColor(Color _color)
-        {
-            //Chama a função SetColor em todos objetos que possuírem a mesma
-            SetColor(_color);
-        }
-		*/
+		
         //Função pública que faz o sorteio e alteração da variável playerColor
         public void ChangeColor(Color _color)
         {
-			//playerColor = RandomColor();
 			playerColor = _color;
         }
 
