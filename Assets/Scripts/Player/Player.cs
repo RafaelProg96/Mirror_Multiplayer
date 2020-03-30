03d3531380f850135ab563760f5a167320279f35
vogtguilherme <vogt.guilherme@gmail.com>
2020-03-29 23:19:31 -0300

    Camera do jogador

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    [SerializeField]private Camera playerCamera;
+    [SerializeField]private GameObject cameraPrefab;
+    [SerializeField]private Transform cameraPosition;
+
+    void Start()
+    {
+        if(!isLocalPlayer)   
+        {
+            return;
+        }
+
+        SetupCamera();
+    }
+
+    void SetupCamera()
+    {
+        var camera = Instantiate(cameraPrefab, cameraPosition);
+
+        camera.transform.rotation = cameraPosition.rotation;
+
+        camera.tag = "MainCamera";
+
+        playerCamera = camera.GetComponent<Camera>();
+    }
+}
