a22fa0a2ff05d4aa27ff93745d2cd67817542b61
Ângelo <angzdm02@gmail.com>
2020-06-16 16:39:54 -0300

    Atualização dos pontos

---
+++ Assets/Futebol/Scripts/UIScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    public Text teamAPointsText, teamBPointsText, colum;
+
+    private void Start()
+    {
+        teamAPointsText.color = Color.magenta;
+        teamBPointsText.color = Color.yellow;
+        
+        colum.color = Color.cyan;
+        colum.text = ":";
+
+        UpdateScoreText(0,0);
+    }
+
+    public void UpdateScoreText(int teamAPoints, int teamBPoints)
+    {
+        teamAPointsText.text = teamAPoints.ToString();
+        teamBPointsText.text = teamBPoints.ToString();
+    }
+}
