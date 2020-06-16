c3a6ddc4cb2c282c5985ee3808193c2f07f3692f
RafaelProg96 <55996912+RafaelProg96@users.noreply.github.com>
2020-06-16 19:02:24 -0300

    Add files via upload

---
+++ Futebol/Scripts/Menu/Buttons.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    //Reference to the menu panels
+    public GameObject howToPlay;
+    public GameObject menu;
+
+   public void Play()
+    {
+        //Load "Lobby" scene
+        SceneManager.LoadScene("Lobby");
+    }
+
+    public void Instructions()
+    {
+        //Deactivate menu panel
+        menu.SetActive(false);
+        //Activate instructions panel
+        howToPlay.SetActive(true);
+    }
+
+    public void Menu()
+    {
+        //Deactivate instructions panel
+        howToPlay.SetActive(false);
+        //Activate menu panel
+        menu.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
