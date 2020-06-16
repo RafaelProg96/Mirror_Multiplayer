77997a193ba5dc590a3a96782ed4b258fea3dfb9
PITARA <pitarapey@gmail.com>
2020-06-16 16:25:31 -0300

    Implementação de movimentação diferenciada

---
+++ Assets/Futebol/Scripts/PlayerMovementation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementation : MonoBehaviour
+{
+    private int speed = 10;
+    // Player movement speed
+
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+    }
+}
