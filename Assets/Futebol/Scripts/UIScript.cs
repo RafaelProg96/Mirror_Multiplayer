d1c2923e03a6b6b1cef19f5342b618c2e14ba1ba
Ângelo <angzdm02@gmail.com>
2020-06-16 17:56:30 -0300

    Timer

--- Assets/Futebol/Scripts/UIScript.cs
+++ Assets/Futebol/Scripts/UIScript.cs
@@ -5,22 +5,55 @@ using UnityEngine.UI;
 
 public class UIScript : MonoBehaviour
 {
-    public Text teamAPointsText, teamBPointsText, colum;
+    public Text teamAPointsText, teamBPointsText, columText, timerText;
+    private readonly int initialTimer = 120;
+    private float timer;
 
     private void Start()
     {
         teamAPointsText.color = Color.magenta;
         teamBPointsText.color = Color.yellow;
         
-        colum.color = Color.cyan;
-        colum.text = ":";
+        columText.color = Color.cyan;
+        columText.text = ":";
 
-        UpdateScoreText(0,0);
+        UpdateScoreText(0, 0);
+
+        timer = initialTimer;
+        StartTimer();
     }
 
     public void UpdateScoreText(int teamAPoints, int teamBPoints)
     {
         teamAPointsText.text = teamAPoints.ToString();
         teamBPointsText.text = teamBPoints.ToString();
+    }
+
+    public void UpdateTimerText(string time)
+    {
+        timerText.text = time;
+    }
+
+    public void StartTimer()
+    {
+        StartCoroutine(Timer());
+    }
+
+    public IEnumerator Timer()
+    {
+        float minutes = timer / 60f;
+        minutes = (int)minutes;
+
+        float seconds = timer % 60f;
+
+        string time = minutes.ToString("f0") + ":" + seconds.ToString("00");
+        UpdateTimerText(time);
+
+        yield return new WaitForSeconds(1f);
+
+        timer--;
+
+        if (timer > 0) { StartTimer(); }
+        else { yield return null; }
     }
 }
